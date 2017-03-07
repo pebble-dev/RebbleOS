785e9e8271a094ce9b08b9bc73628ea56937a7be
Joshua Wise <joshua@joshuawise.com>
2017-03-07 10:28:06 -0800

    tintin: fix buttons

--- hw/platform/tintin/tintin.c
+++ hw/platform/tintin/tintin.c
@@ -116,7 +116,7 @@ void hw_backlight_set(uint16_t val) {
 void hw_buttons_init() {
 }
 
-uint8_t hw_button_pressed(uint_t button_id) {
+uint8_t hw_button_pressed(uint8_t button_id) {
 	return 0;
 }
 
