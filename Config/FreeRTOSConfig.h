490fabfbec5a09d90979576f3cb5afd668fe7c8f
Joshua Wise <joshua@joshuawise.com>
2017-04-24 13:51:39 +0200

    FreeRTOS: config: turn up the minimal stack size for a little while, at least until we get to dealign with this the right way later

--- Config/FreeRTOSConfig.h
+++ Config/FreeRTOSConfig.h
@@ -97,7 +97,7 @@
 #define configCPU_CLOCK_HZ    ( SystemCoreClock )
 #define configTICK_RATE_HZ    ( ( TickType_t ) 200 )
 #define configMAX_PRIORITIES   ( 5 )
-#define configMINIMAL_STACK_SIZE  ( ( unsigned short ) 130 )
+#define configMINIMAL_STACK_SIZE  ( ( unsigned short ) 300 )
 #define configTOTAL_HEAP_SIZE   ( ( size_t ) ( 75 * 1024 ) )
 #define configMAX_TASK_NAME_LEN   ( 10 )
 #define configUSE_TRACE_FACILITY  0
