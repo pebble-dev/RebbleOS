79c9d83149a46278cd993b477f9e671efe3593e5
Bogdan Olar <olar.bogdan.dev@gmail.com>
2017-04-23 20:25:36 -0400

    Added a simple API to the vibrate module.

--- RebbleOS/vibrate.c
+++ RebbleOS/vibrate.c
@@ -25,43 +25,41 @@
 #include "semphr.h"
 #include <stdbool.h>
 
-typedef struct
-{
-   const VibrateCmd_t command;
-   size_t curBufferIndex;
-   const size_t length;
-   const uint16_t * const buffer;
-} VibrateTrack_t;
+#define VIBRATE_QUEUE_MAX_WAIT_TICKS  20    // chosen by fair dice roll
+
 
 /*
- * Definitions of all tracks.
+ * Initialization of default patterns.
  * 
- * TODO Dinamically load these.
+ * TODO Maybe load these dinamically at some point
  */
-static VibrateTrack_t _vibrateTracks[VIBRATE_CMD_MAX] = {
-   {.command = VIBRATE_CMD_STOP     , .length = 1, .buffer = (const uint16_t []) {100}},
-   {.command = VIBRATE_CMD_PATTERN_1, .length = 2, .buffer = (const uint16_t []) {255, 1000}},
-   {.command = VIBRATE_CMD_PATTERN_2, .length = 6, .buffer = (const uint16_t []) {255, 750, 0, 750, 255, 750}},
+static VibratePattern_t _defaultVibratePatterns[VIBRATE_CMD_MAX] = {
+    {.length =  2, .buffer = (const uint16_t []) {255, 1000}},                   // VIBRATE_CMD_PLAY_PATTERN_1
+    {.length =  6, .buffer = (const uint16_t []) {255, 750, 0, 750, 255, 750}},  // VIBRATE_CMD_PALY_PATTERN_2
+    {.length = 30, .buffer = (const uint16_t []) {255, 750, 0, 750, 255, 750,
+                                                  255, 750, 0, 750, 255, 750, 
+                                                  255, 750, 0, 750, 255, 750, 
+                                                  255, 750, 0, 750, 255, 750, 
+                                                  255, 750, 0, 750, 255, 750}},  // VIBRATE_CMD_PALY_PATTERN_3
+    {.length =  0, .buffer = (const uint16_t []) {}},                          // VIBRATE_CMD_STOP
 };
 
 static TaskHandle_t _vibrate_task;
 static xQueueHandle _vibrate_queue;
+static VibratePattern_t *currentPattern;
 
-static void _vibrate_thread(void *pvParameters);
 static void _enable(uint8_t enabled);
-static void _play_track(VibrateTrack_t *pattern, bool doRepeat);
-static void _rewind_track(VibrateTrack_t* pattern);
-static void _rewind_all_tracks(void);
+static void _play(void);
+static void _vibrate_thread(void *pvParameters);
 
 /*
- * Initialise the vibration controller and tasks
+ * Initialize the vibration controller and tasks
  */
 void vibrate_init(void)
 {
     int rv;
-        
+    
     hw_vibrate_init();
-    _rewind_all_tracks();
     
     rv = xTaskCreate(_vibrate_thread, "Vibrate", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2UL, &_vibrate_task); /* XXX: allocate statically later */
     assert(rv == pdPASS);
@@ -69,7 +67,44 @@ void vibrate_init(void)
     _vibrate_queue = xQueueCreate(1, sizeof(uint8_t));
 }
 
-/*
+/**
+ * Execute a command
+ * @param command Command to execute. Note that VIBRATE_CMD_MAX is not a valid command.
+ */
+void vibrate_command(VibrateCmd_t command)
+{
+    if (command < VIBRATE_CMD_MAX)
+    {
+        vibrate_play_pattern(&_defaultVibratePatterns[command]);
+    }
+    else
+    {
+        // ya dun goofed
+        assert(false);
+    }
+}
+
+/**
+ * Play a given pattern.
+ * @param pattern Pointer to a struct containing a defined pattern.
+ */
+void vibrate_play_pattern(VibratePattern_t *pattern)
+{
+    pattern->curBufferIndex = 0;
+    (void) xQueueSendToBack(_vibrate_queue, &pattern, VIBRATE_QUEUE_MAX_WAIT_TICKS);   
+}
+
+/**
+ * Stop playing a pattern. If the _vibrate_thread is currently blocked by vTaskDelay,
+ * the pattern will be stopped when vTaskDelay returns.
+ */
+void vibrate_stop(void)
+{
+    
+    (void) xQueueSendToBack(_vibrate_queue, &_defaultVibratePatterns[VIBRATE_CMD_STOP], VIBRATE_QUEUE_MAX_WAIT_TICKS);    
+}
+
+/**
  * Start vibratin
  */
 static void _enable(uint8_t enabled)
@@ -77,48 +112,36 @@ static void _enable(uint8_t enabled)
     hw_vibrate_enable(enabled);
 }
 
-/*
- * Play a given pattern.
+
+/**
+ * Play the currently selected pattern.
  */
-static void _play_track(VibrateTrack_t *pattern, bool doRepeat)
+static void _play()
 {
-    assert(pattern->curBufferIndex <= pattern->length);
-      
-    if(pattern->curBufferIndex < pattern->length)
-    {
-        if (pattern->buffer[pattern->curBufferIndex] > 0)
+    while (currentPattern->curBufferIndex < currentPattern->length)
+    {        
+        if (currentPattern->buffer[currentPattern->curBufferIndex] > 0)
         {
             _enable(true);
         }
-       
-        vTaskDelay(pattern->buffer[pattern->curBufferIndex] / portTICK_RATE_MS);
-       
+        
+        vTaskDelay(currentPattern->buffer[currentPattern->curBufferIndex] / portTICK_RATE_MS);
         _enable(false);
-        pattern->curBufferIndex++;
+        
+        
+        if (xQueueReceive(_vibrate_queue, &currentPattern, 0))
+        {
+            // if we just received another pattern (including stop),
+            // start playing it from the beginning
+            currentPattern->curBufferIndex = 0;
+        }
+        else
+        {
+            currentPattern->curBufferIndex++;
+        }
     }
-    else if (doRepeat)
-    {
-        pattern->curBufferIndex = 0;
-    }
-}
-
-/*
- * Reset all patterns to their default state
- */
-static void _rewind_all_tracks()
-{
-    for(VibrateCmd_t i = 0; i < VIBRATE_CMD_MAX; i++)
-    {
-        _vibrateTracks[i].curBufferIndex = 0;
-    }
-}
-
-/*
- * Set a track's position to 0
- */
-static void _rewind_track(VibrateTrack_t* pattern)
-{
-    pattern->curBufferIndex = 0;
+    
+    currentPattern = &_defaultVibratePatterns[VIBRATE_CMD_STOP];
 }
 
 /*
@@ -126,20 +149,15 @@ static void _rewind_track(VibrateTrack_t* pattern)
  */
 static void _vibrate_thread(void *pvParameters)
 {
-    VibrateCmd_t command;
-    static VibrateTrack_t *currentTrack = &_vibrateTracks[VIBRATE_CMD_STOP];
-   
+    currentPattern = &_defaultVibratePatterns[VIBRATE_CMD_STOP];
+    
     while(1)
-    {       
-        if (xQueueReceive(_vibrate_queue, &command, portMAX_DELAY))
+    {
+        if (!xQueueReceive(_vibrate_queue, &currentPattern, portMAX_DELAY))
         {
-            if (command < VIBRATE_CMD_MAX)
-            {
-                currentTrack = &_vibrateTracks[command];
-                _rewind_track(currentTrack);
-            }
+            continue;
         }
-         
-        _play_track(currentTrack, false);
+        
+        _play();
     }
 }
--- RebbleOS/vibrate.h
+++ RebbleOS/vibrate.h
@@ -17,16 +17,33 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * All possible commands (with the exception of VIBRATE_CMD_MAX, which is only used to determine
+ * the size of the array containing the default patterns)
+ */
 typedef enum
 {
-   VIBRATE_CMD_STOP,
-   VIBRATE_CMD_PATTERN_1,
-   VIBRATE_CMD_PATTERN_2,
-   VIBRATE_CMD_MAX,        // not an actual command; used as a maximum index or sentinel value
+    VIBRATE_CMD_PLAY_PATTERN_1,
+    VIBRATE_CMD_PALY_PATTERN_2,
+    VIBRATE_CMD_PALY_PATTERN_3,
+    VIBRATE_CMD_STOP,
+    VIBRATE_CMD_MAX // add any other commands IDs _before_ this
 } VibrateCmd_t;
 
-// #define VIBRATE_CMD_PATTERN_1 1
-// #define VIBRATE_CMD_PATTERN_2 2
-// #define VIBRATE_CMD_STOP      3
+/**
+ * Struct which defines a pattern:
+ *  buffer - contains the sequence of durations
+ *  length - is the length of the buffer (a duration of 0 will be interpreted as silence)
+ *  curBufferIndex - the index in the buffer of the current durration
+ */
+typedef struct
+{
+    const uint8_t length;
+    const uint16_t * const buffer;
+    uint8_t curBufferIndex;
+} VibratePattern_t;
 
 void vibrate_init(void);
+void vibrate_command(VibrateCmd_t command);
+void vibrate_play_pattern(VibratePattern_t *pattern);
+void vibrate_stop(void);
