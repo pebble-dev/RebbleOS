3032796830dbe0f15ed0d24fd351488a52d3b18e
Stubenhocker <Stubenhocker1399@gmail.com>
2018-01-27 01:22:33 +1000

    Rename static variables

--- Apps/System/music.c
+++ Apps/System/music.c
@@ -23,18 +23,19 @@
 #define ARM_HOME_ANGLE 0
 #define ARM_START_ANGLE 18
 #define ARM_END_ANGLE 46
-#define INITIAL_ARM_SKIP_SPEED 200
+#define ARM_SKIP_SPEED 200
 #define ARM_QUICK_SPEED 100
 #define SKIP_DIRECTION_PREV -1
 #define SKIP_DIRECTION_NEXT 1
+
 static Window *s_main_window;
 static Layer *s_main_layer;
-ActionBarLayer *s_action_bar;
+static ActionBarLayer *s_action_bar;
 
-static n_GPath *arm_base_path_ptr;
-static n_GPath *arm_1_path_ptr;
-static n_GPath *arm_2_path_ptr;
-static n_GPath *arm_head_path_ptr;
+static n_GPath *s_arm_base_path_ptr;
+static n_GPath *s_arm_1_path_ptr;
+static n_GPath *s_arm_2_path_ptr;
+static n_GPath *s_arm_head_path_ptr;
 
 static const n_GPathInfo ARM_BASE_PATH_INFO = {
     .num_points = 2,
@@ -53,29 +54,27 @@ static const n_GPathInfo ARM_HEAD_PATH_INFO = {
     .points = (n_GPoint[]) {{19, -3-19-14}, {19+9, -3-19-15-6}}
 };
 
-GBitmap *s_up_bitmap;
-GBitmap *s_down_bitmap;
-GBitmap *s_select_bitmap;
-
-int progress_pixels;
-int32_t skip_value;
-char *curr_artist;
-char *curr_track;
-char *curr_progress;
-char *curr_length;
-GColor curr_disk_color;
-GColor next_disk_color;
-GPoint curr_disk_pos;
-GPoint next_disk_pos;
-bool animating_disk_change;
-bool animating_arm_change;
-Animation *s_animation_record_ptr;
-Animation *s_animation_arm_ptr;
-int32_t arm_angle;
-int32_t old_arm_angle;
-int32_t animation_start_arm_angle;
-int32_t animation_end_arm_angle;
-
+static Animation *s_animation_record_ptr;
+static Animation *s_animation_arm_ptr;
+static bool s_animating_disk_change;
+static bool s_animating_arm_change;
+static char *s_artist;
+static char *s_track;
+static char *s_progress;
+static char *s_length;
+static GBitmap *s_up_bitmap;
+static GBitmap *s_down_bitmap;
+static GBitmap *s_select_bitmap;
+static GColor s_curr_disk_color;
+static GColor s_next_disk_color;
+static GPoint s_curr_disk_pos;
+static GPoint s_next_disk_pos;
+static int32_t s_progress_pixels;
+static int32_t s_skip_value;
+static int32_t s_arm_angle;
+static int32_t s_old_arm_angle;
+static int32_t s_animation_start_arm_angle;
+static int32_t s_animation_end_arm_angle;
 static struct tm s_last_time;
 
 static void status_tick(struct tm *tick_time, TimeUnits tick_units ){
@@ -85,49 +84,49 @@ static void status_tick(struct tm *tick_time, TimeUnits tick_units ){
 }
 
 static void implementation_record_setup(Animation *animation) {
-    animating_disk_change = true;
+    s_animating_disk_change = true;
     // TODO set next_disk_color based on title + artist,
     // to let each track have the same color between sessions
     // Avoid color black and white (Changing the stroke color would be an alternative)
-    next_disk_color =  (GColor8) { .argb = ((rand() % 0b00111111) + 0b11000000)  };
+    s_next_disk_color =  (GColor8) { .argb = ((rand() % 0b00111111) + 0b11000000)  };
 
     // Set the initial positions for our two animated records
-    if(skip_value > 0){
-        next_disk_pos = GPoint(RECORD_CENTER_X + RECORD_SWOOSH_DISTANCE, RECORD_CENTER_Y);
+    if(s_skip_value > 0){
+        s_next_disk_pos = GPoint(RECORD_CENTER_X + RECORD_SWOOSH_DISTANCE, RECORD_CENTER_Y);
     } else {
-        next_disk_pos = GPoint(RECORD_CENTER_X - RECORD_SWOOSH_DISTANCE, RECORD_CENTER_Y);
+        s_next_disk_pos = GPoint(RECORD_CENTER_X - RECORD_SWOOSH_DISTANCE, RECORD_CENTER_Y);
     }
 }
 
 static void implementation_record_update(Animation *animation,
                                   const AnimationProgress distance_normalized) {
-    if(skip_value > 0) {
-        curr_disk_pos = GPoint(ANIM_LERP(curr_disk_pos.x, RECORD_CENTER_X - RECORD_SWOOSH_DISTANCE, distance_normalized), RECORD_CENTER_Y);
+    if(s_skip_value > 0) {
+        s_curr_disk_pos = GPoint(ANIM_LERP(s_curr_disk_pos.x, RECORD_CENTER_X - RECORD_SWOOSH_DISTANCE, distance_normalized), RECORD_CENTER_Y);
     } else {
-        curr_disk_pos = GPoint(ANIM_LERP(curr_disk_pos.x, RECORD_CENTER_X + RECORD_SWOOSH_DISTANCE, distance_normalized), RECORD_CENTER_Y);
+        s_curr_disk_pos = GPoint(ANIM_LERP(s_curr_disk_pos.x, RECORD_CENTER_X + RECORD_SWOOSH_DISTANCE, distance_normalized), RECORD_CENTER_Y);
     }
-    next_disk_pos =     GPoint(ANIM_LERP(next_disk_pos.x, RECORD_CENTER_X, distance_normalized),                          RECORD_CENTER_Y);
+    s_next_disk_pos =     GPoint(ANIM_LERP(s_next_disk_pos.x, RECORD_CENTER_X, distance_normalized),                          RECORD_CENTER_Y);
     layer_mark_dirty(s_main_layer);
 }
 
 static void implementation_record_teardown(Animation *animation) {
     // Now that the animation is done, our next disk becomes the current one
-    curr_disk_color = next_disk_color;
-    curr_disk_pos = next_disk_pos;
+    s_curr_disk_color = s_next_disk_color;
+    s_curr_disk_pos = s_next_disk_pos;
     // We successfully skipped, reduce the skip value
-    if (skip_value > 0) {
-        skip_value--;
-    } else if (skip_value < 0) {
-        skip_value++;
+    if (s_skip_value > 0) {
+        s_skip_value--;
+    } else if (s_skip_value < 0) {
+        s_skip_value++;
     }
-    if(skip_value != 0) {
+    if(s_skip_value != 0) {
         // There's more skipping to be done, just leave the arm and continue swapping
         animation_record();
     } else {   
         // We are done, move the arm to the start position
         // TODO actually dont when paused
-        animating_disk_change = false;
-        animating_arm_change = true;
+        s_animating_disk_change = false;
+        s_animating_arm_change = true;
         animation_arm(ARM_START_ANGLE, ARM_QUICK_SPEED);
     }
 }
@@ -137,20 +136,20 @@ static void animation_record() {
 }
 
 static void implementation_arm_setup(Animation *animation) {
-    animation_start_arm_angle = arm_angle;
-    animating_arm_change = true;
+    s_animation_start_arm_angle = s_arm_angle;
+    s_animating_arm_change = true;
 }
 
 static void implementation_arm_update(Animation *animation,
                                       const AnimationProgress distance_normalized) {
-    arm_angle = ANIM_LERP(animation_start_arm_angle, animation_end_arm_angle, distance_normalized);
+    s_arm_angle = ANIM_LERP(s_animation_start_arm_angle, s_animation_end_arm_angle, distance_normalized);
     layer_mark_dirty(s_main_layer);
 }
 
 static void implementation_arm_teardown(Animation *animation) {
-    animating_arm_change = false;
-    if(skip_value != 0) {
-        if(arm_angle != ARM_HOME_ANGLE) {
+    s_animating_arm_change = false;
+    if(s_skip_value != 0) {
+        if(s_arm_angle != ARM_HOME_ANGLE) {
             // Skip value is not 0, so it means we are still skipping,
             // we are not at home, so quickly move the arm to make room
             animation_arm(ARM_HOME_ANGLE, ARM_QUICK_SPEED);
@@ -163,17 +162,17 @@ static void implementation_arm_teardown(Animation *animation) {
 
 static void animation_arm(int32_t angle, uint32_t duration_ms) {
     // TODO Instead of taking a fixed duration set a duration based on the distance
-    animation_end_arm_angle = angle;
+    s_animation_end_arm_angle = angle;
     animation_set_duration(s_animation_arm_ptr, duration_ms);
     animation_schedule(s_animation_arm_ptr);
 }
 
 static void skip_track(int32_t direction) {
     // Only start the animation when we are not already animating
-    if(skip_value == 0 && !animating_disk_change && !animating_arm_change) {
-        animation_arm(ARM_HOME_ANGLE, INITIAL_ARM_SKIP_SPEED);
+    if(s_skip_value == 0 && !s_animating_disk_change && !s_animating_arm_change) {
+        animation_arm(ARM_HOME_ANGLE, ARM_SKIP_SPEED);
     }
-    skip_value += direction;
+    s_skip_value += direction;
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -219,39 +218,39 @@ void draw_record(GContext *ctx, GPoint record_center_point, GColor record_color)
 }
 
 void destroy_paths() {
-    n_gpath_destroy(arm_base_path_ptr);
-    n_gpath_destroy(arm_1_path_ptr);
-    n_gpath_destroy(arm_2_path_ptr);
-    n_gpath_destroy(arm_head_path_ptr);
+    n_gpath_destroy(s_arm_base_path_ptr);
+    n_gpath_destroy(s_arm_1_path_ptr);
+    n_gpath_destroy(s_arm_2_path_ptr);
+    n_gpath_destroy(s_arm_head_path_ptr);
 }
 
 void draw_arm(GContext *ctx, int32_t angle) {
-    if(angle != old_arm_angle) {
+    if(angle != s_old_arm_angle) {
         // Only recalculate the points if the angle has changed
         destroy_paths();
-        arm_base_path_ptr = n_gpath_create(&ARM_BASE_PATH_INFO);
-        arm_1_path_ptr    = n_gpath_create(&ARM_1_PATH_INFO);
-        arm_2_path_ptr    = n_gpath_create(&ARM_2_PATH_INFO);
-        arm_head_path_ptr = n_gpath_create(&ARM_HEAD_PATH_INFO);
-        n_gpath_move_to(arm_base_path_ptr, ARM_OFFSET);
-        n_gpath_move_to(arm_1_path_ptr,    ARM_OFFSET);
-        n_gpath_move_to(arm_2_path_ptr,    ARM_OFFSET);
-        n_gpath_move_to(arm_head_path_ptr, ARM_OFFSET);
+        s_arm_base_path_ptr = n_gpath_create(&ARM_BASE_PATH_INFO);
+        s_arm_1_path_ptr    = n_gpath_create(&ARM_1_PATH_INFO);
+        s_arm_2_path_ptr    = n_gpath_create(&ARM_2_PATH_INFO);
+        s_arm_head_path_ptr = n_gpath_create(&ARM_HEAD_PATH_INFO);
+        n_gpath_move_to(s_arm_base_path_ptr, ARM_OFFSET);
+        n_gpath_move_to(s_arm_1_path_ptr,    ARM_OFFSET);
+        n_gpath_move_to(s_arm_2_path_ptr,    ARM_OFFSET);
+        n_gpath_move_to(s_arm_head_path_ptr, ARM_OFFSET);
         int32_t calculated_angle = TRIG_MAX_ANGLE / 360 * (angle + ARM_OFFSET_ANGLE);
-        n_gpath_rotate_to(arm_base_path_ptr, calculated_angle);
-        n_gpath_rotate_to(arm_1_path_ptr,    calculated_angle);
-        n_gpath_rotate_to(arm_2_path_ptr,    calculated_angle);
-        n_gpath_rotate_to(arm_head_path_ptr, calculated_angle);
-        old_arm_angle = arm_angle;
+        n_gpath_rotate_to(s_arm_base_path_ptr, calculated_angle);
+        n_gpath_rotate_to(s_arm_1_path_ptr,    calculated_angle);
+        n_gpath_rotate_to(s_arm_2_path_ptr,    calculated_angle);
+        n_gpath_rotate_to(s_arm_head_path_ptr, calculated_angle);
+        s_old_arm_angle = s_arm_angle;
     }
     n_graphics_context_set_stroke_caps(ctx, false);
     n_graphics_context_set_stroke_width(ctx, 5);
-    n_gpath_draw(ctx, arm_base_path_ptr);
+    n_gpath_draw(ctx, s_arm_base_path_ptr);
     n_graphics_context_set_stroke_width(ctx, 1);
-    n_gpath_draw(ctx, arm_1_path_ptr);
-    n_gpath_draw(ctx, arm_2_path_ptr);
+    n_gpath_draw(ctx, s_arm_1_path_ptr);
+    n_gpath_draw(ctx, s_arm_2_path_ptr);
     n_graphics_context_set_stroke_width(ctx, 4);    
-    n_gpath_draw(ctx, arm_head_path_ptr);
+    n_gpath_draw(ctx, s_arm_head_path_ptr);
 }
 
 static void main_layer_update_proc(Layer *layer, GContext *ctx) {
@@ -270,30 +269,30 @@ static void main_layer_update_proc(Layer *layer, GContext *ctx) {
     
     // Draw text
     graphics_context_set_text_color(ctx, GColorBlack);
-    graphics_draw_text(ctx, curr_artist, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(4, 108, 105, 10), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, curr_track, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD), GRect(4, 117, 110, 50), n_GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
+    graphics_draw_text(ctx, s_artist, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(4, 108, 105, 10), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
+    graphics_draw_text(ctx, s_track, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD), GRect(4, 117, 110, 50), n_GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
     
-    graphics_draw_text(ctx, curr_progress, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect( 4, 84, (144 - 30) / 2 + 4, 10), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, curr_length, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect((144 - 30) / 2, 84, (144 - 30) / 2 - 4, 10), GTextOverflowModeTrailingEllipsis, GTextAlignmentRight, NULL);
+    graphics_draw_text(ctx, s_progress, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect( 4, 84, (144 - 30) / 2 + 4, 10), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
+    graphics_draw_text(ctx, s_length, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect((144 - 30) / 2, 84, (144 - 30) / 2 - 4, 10), GTextOverflowModeTrailingEllipsis, GTextAlignmentRight, NULL);
     
     char time_string[8] = "";
     strftime(time_string, 8, "%R", &s_last_time);
 
     graphics_draw_text(ctx, time_string, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(0, 0, 113, 10), GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
     
-    if (animating_disk_change) {
-        draw_arm(ctx, arm_angle);
-        draw_record(ctx, next_disk_pos, next_disk_color);
+    if (s_animating_disk_change) {
+        draw_arm(ctx, s_arm_angle);
+        draw_record(ctx, s_next_disk_pos, s_next_disk_color);
     }
-    draw_record(ctx, curr_disk_pos, curr_disk_color);
+    draw_record(ctx, s_curr_disk_pos, s_curr_disk_color);
 
-    if(!animating_disk_change) {
-        draw_arm(ctx, arm_angle);
+    if(!s_animating_disk_change) {
+        draw_arm(ctx, s_arm_angle);
     }
     // Draw bar
     graphics_fill_rect(ctx, GRect(4, 102, 105, 6), 1, n_GCornersAll);
-    graphics_context_set_fill_color(ctx, curr_disk_color);
-    graphics_fill_rect(ctx, GRect(5, 103, progress_pixels, 4), 0, GCornerNone);
+    graphics_context_set_fill_color(ctx, s_curr_disk_color);
+    graphics_fill_rect(ctx, GRect(5, 103, s_progress_pixels, 4), 0, GCornerNone);
 }
 
 static void music_window_load(Window *window) {
@@ -302,16 +301,16 @@ static void music_window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(s_main_window);
     GRect bounds = layer_get_unobstructed_bounds(window_layer);
 
-    curr_disk_color = GColorWindsorTan;
-    curr_disk_pos = RECORD_CENTER;
-    progress_pixels = 25;
-    curr_artist = "The Beatles";
-    curr_track = "Maxwell's Silver Hammer";
-    curr_length = "3:27";
-    curr_progress = "0:44";
-    arm_angle = ARM_END_ANGLE;
-    old_arm_angle = -1;
-    skip_value = 0;
+    s_curr_disk_color = GColorWindsorTan;
+    s_curr_disk_pos = RECORD_CENTER;
+    s_progress_pixels = 25;
+    s_artist = "The Beatles";
+    s_track = "Maxwell's Silver Hammer";
+    s_length = "3:27";
+    s_progress = "0:44";
+    s_arm_angle = ARM_END_ANGLE;
+    s_old_arm_angle = -1;
+    s_skip_value = 0;
     s_main_layer = layer_create(bounds);
     layer_add_child(window_layer, s_main_layer);
 
