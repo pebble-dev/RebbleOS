0473264f468a808f6defbaef2e6527f9c4684996
Barry Carter <barry.carter@gmail.com>
2018-02-16 23:21:32 +0000

    Tintin setup clocks

--- hw/chip/stm32f2xx/system_stm32f2xx.c
+++ hw/chip/stm32f2xx/system_stm32f2xx.c
@@ -148,14 +148,14 @@
 
 
 /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N */
-#define PLL_M      25
-#define PLL_N      240
+#define PLL_M      8
+#define PLL_N      192
 
 /* SYSCLK = PLL_VCO / PLL_P */
-#define PLL_P      2
+#define PLL_P      6
 
 /* USB OTG FS, SDIO and RNG Clock =  PLL_VCO / PLLQ */
-#define PLL_Q      5
+#define PLL_Q      8
 
 /**
   * @}
@@ -173,7 +173,7 @@
   * @{
   */
 
-  uint32_t SystemCoreClock = 120000000;
+  uint32_t SystemCoreClock = 64000000;
 
   __I uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
 
