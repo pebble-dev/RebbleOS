570dcdc72e6823b8552a8b596650ccd88c9d8a08
Joshua Wise <joshua@joshuawise.com>
2019-03-31 15:07:02 -0700

    asterix: Switch flash to be IRQ-driven, and prepare to actually have flash contents.

--- hw/platform/asterix/asterix_flash.c
+++ hw/platform/asterix/asterix_flash.c
@@ -9,6 +9,9 @@
 #include "rebbleos.h"
 #include "nrfx_qspi.h"
 
+/* Asterix has 128Mbit (16MB) of QSPI flash -- a W25Q128JV. */
+#define QSPI_JEDEC_ID_W25Q128JV 0xEF4018
+
 #define BOARD_QSPI_SCK_PIN 19
 #define BOARD_QSPI_CSN_PIN 17
 #define BOARD_QSPI_IO0_PIN 20
@@ -17,6 +20,12 @@
 #define BOARD_QSPI_IO3_PIN 23
 
 #define QSPI_INSTR_JEDEC_ID 0x9F
+
+static void _flash_handler(nrfx_qspi_evt_t event, void *ctx) {
+    assert(event == NRFX_QSPI_EVENT_DONE);
+    
+    flash_operation_complete_isr(0);
+}
 
 void hw_flash_init() {
     nrfx_qspi_config_t config = NRFX_QSPI_DEFAULT_CONFIG;
@@ -27,9 +36,10 @@ void hw_flash_init() {
     config.pins.io1_pin = BOARD_QSPI_IO1_PIN;
     config.pins.io2_pin = BOARD_QSPI_IO2_PIN;
     config.pins.io3_pin = BOARD_QSPI_IO3_PIN;
+    config.irq_priority = configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY + 1;
 
     nrfx_err_t err;
-    err = nrfx_qspi_init(&config, NULL, NULL);
+    err = nrfx_qspi_init(&config, _flash_handler, NULL);
     assert(err == NRFX_SUCCESS && "QSPI initialization failed");
 
     /* Read the JEDEC ID out of the flash */    
@@ -39,12 +49,15 @@ void hw_flash_init() {
     assert(err == NRFX_SUCCESS && "QSPI JEDEC ID read failed");
 
     DRV_LOG("flash", APP_LOG_LEVEL_DEBUG, "QSPI: JEDEC ID %02x %02x %02x", buf[0], buf[1], buf[2]);
+    
+    assert((buf[0] == ((QSPI_JEDEC_ID_W25Q128JV >> 16) & 0xFF)) &&
+           (buf[1] == ((QSPI_JEDEC_ID_W25Q128JV >>  8) & 0xFF)) &&
+           (buf[2] == ((QSPI_JEDEC_ID_W25Q128JV      ) & 0xFF)));
 }
 
 void hw_flash_read_bytes(uint32_t addr, uint8_t *buf, size_t len) {
     nrfx_err_t err;
 
-    /* XXX: could operate from an interrupt, I suppose */
     err = nrfx_qspi_read(buf, len, addr);
-    flash_operation_complete(err != NRFX_SUCCESS);
+    assert(err == NRFX_SUCCESS);
 }
--- hw/platform/asterix/platform.h
+++ hw/platform/asterix/platform.h
@@ -6,52 +6,30 @@
 #define DISPLAY_ROWS 168
 #define DISPLAY_COLS 144
 
-#define REGION_PRF_START    0x200000
-#define REGION_PRF_SIZE     0x1000000
-// DO NOT WRITE TO THIS REGION
-#define REGION_MFG_START    0xE0000
-#define REGION_MFG_SIZE     0x20000
-// Resource start
-#define REGION_RES_START    0x280000
-#define REGION_RES_SIZE     0x7D000
+/* Asterix has 16MB of flash. */
 
-#define REGION_FS_START         0x2c0000
+/* Bootloader private area: 1MB */
+#define REGION_BOOTLOADER_START 0x0
+#define REGION_BOOTLOADER_SIZE  0x100000
+
+/* Reflash staging area: 1MB ROM, 1MB resources */
+#define REGION_STAGING_START 0x100000
+#define REGION_STAGING_SIZE  0x200000
+
+/* System resources: 1MB */
+#define REGION_RES_START 0x300000
+#define REGION_RES_SIZE  0x100000
+
+/* The rest of the filesystem: 12MB */
+#define REGION_FS_START         0x400000
 #define REGION_FS_PAGE_SIZE     0x1000
-#define REGION_FS_N_PAGES       ((0x3E0000 - REGION_FS_START) / REGION_FS_PAGE_SIZE)
-
-#define REGION_APP_RES_START    0xB3A000
-#define REGION_APP_RES_SIZE     0x7D000
-
-// XXX TODO these are from Snowy. NOT correct
-/* App slots are a chunk of flash that holds the information.
-    Seems to be paged
-*/
-#define APP_SLOT_0_START        0xB3E000
-#define APP_SLOT_8_START        0xc34000
-#define APP_SLOT_16_START       0xc82000
-#define APP_SLOT_24_START       0xe7e000
-
-
-/* Each app section (resouce, binary) has this header junk */
-#define APP_HEADER_BIN_OFFSET   0x59
-
-/* The size of the slot on flash for an app. */
-#define APP_SLOT_SIZE           0x48000
+#define REGION_FS_N_PAGES       ((0xFE0000 - REGION_FS_START) / REGION_FS_PAGE_SIZE)
 
 /* The size of the page that holds an apps header table. This is the amount before actual app content e.g
  0x0000  Resource table header
  0x1000  Resource data start
  */
 #define APP_RES_START           0x1000
-
-/* This is the NEGATIVE (yes, sigh) offset of the resource table from the app binary */
-#define APP_RES_TABLE_OFFSET    0x6000
-
-/* When we load a font from flash, is it offset from the given offset in the header? Seems so */
-#define APP_FONT_START          0x1C
-
-
-// BACK TO CORRECT?
 
 /* XXX: issue pebble-dev/RebbleOS#43 */
 #define RES_START           0x200C
