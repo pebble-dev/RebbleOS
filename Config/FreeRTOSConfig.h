41634820f29c5c3dd608a11b17da06608073cf69
Joshua Wise <joshua@joshuawise.com>
2019-03-30 21:22:06 -0700

    Use nRF definitions for interrupt priority, to avoid irritating the softdevice.

--- Config/FreeRTOSConfig.h
+++ Config/FreeRTOSConfig.h
@@ -153,7 +153,9 @@
    routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
    INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
    PRIORITY THAN THIS! (higher priorities are lower numeric values. */
+#ifndef configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY
 #define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 5
+#endif
 
 /* Interrupt priorities used by the kernel port layer itself.  These are generic
    to all Cortex-M ports, and do not rely on any particular library functions. */
--- hw/platform/asterix/platform_freertos.h
+++ hw/platform/asterix/platform_freertos.h
@@ -1,6 +1,11 @@
 #ifndef _PLATFORM_FREERTOS_H
 #define _PLATFORM_FREERTOS_H
 
+#include "nrf_soc.h"
+#include "app_util_platform.h"
+
 extern uint32_t SystemCoreClock;
+
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY _PRIO_APP_HIGH
 
 #endif
