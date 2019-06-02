6a090af0694db123b9f3185c8368ce5aeb13a390
Barry Carter <barry.carter@gmail.com>
2019-06-02 21:08:05 +0100

    give the overlay window its own ngfx graphics context.
    Stop apps and overlay painting from affecting each other.
    current pen/background colour now stays as selected

--- rwatch/ngfxwrap.c
+++ rwatch/ngfxwrap.c
@@ -8,6 +8,7 @@
 #include "context.h"
 #include "appmanager.h"
 #include "display.h"
+#include "overlay_manager.h"
 
 void rwatch_neographics_init(app_running_thread *thread)
 {
@@ -17,5 +18,11 @@ void rwatch_neographics_init(app_running_thread *thread)
 n_GContext *rwatch_neographics_get_global_context(void)
 {
     app_running_thread *_this_thread = appmanager_get_current_thread();
+    
+    if (_this_thread->thread_type == AppThreadOverlay)
+    {
+        OverlayWindow *owindow = overlay_stack_get_top_overlay_window();
+        return owindow->graphics_context;
+    }
     return _this_thread->graphics_context;
 }
