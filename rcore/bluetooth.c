88b70ad33a5ceddfbfb6c6198d01a71c307b5330
Joshua Wise <joshua@joshuawise.com>
2019-08-30 15:31:30 +0800

    bluetooth: fill pbl_transport_packet data and length structures (I have no idea how this worked before)

--- rcore/bluetooth.c
+++ rcore/bluetooth.c
@@ -168,6 +168,9 @@ void bluetooth_data_rx(uint8_t *data, size_t len)
 {
     pbl_transport_packet pkt;
     protocol_rx_buffer_append(data, len);
+    
+    pkt.data = protocol_get_rx_buffer();
+    pkt.length = protocol_get_rx_buf_size();
 
     if (!protocol_parse_packet(&pkt, bluetooth_send_data))
         return; // we are done, no point looking as we have no data left
