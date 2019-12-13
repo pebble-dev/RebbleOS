4180cb053fb1cba2d5b573213136a3eae7957aec
Joshua Wise <joshua@joshuawise.com>
2019-12-12 23:26:21 -0500

    Run tests on their own thread.

--- rcore/test.h
+++ rcore/test.h
@@ -14,7 +14,7 @@
 uint8_t test_init(void);
 void test_packet_handler(const pbl_transport_packet *packet);
 
-typedef int (*testfn_t)();
+typedef int (*testfn_t)(uint32_t *artifact);
 #define TESTNAME_LEN 32
 struct test {
     char testname[TESTNAME_LEN];
@@ -22,12 +22,12 @@ struct test {
 };
 
 #define TEST(name) \
-static int test_##name();\
+static int test_##name(uint32_t *artifact);\
 const struct test testdef_##name __attribute__((section(".rodata.tests"))) = { \
     .testname = #name, \
     .testfn = test_##name \
 }; \
-static int test_##name()
+static int test_##name(uint32_t *artifact)
 
 #define TEST_PASS 0
 #define TEST_FAIL -1
@@ -35,7 +35,7 @@ static int test_##name()
 #else
 
 #define TEST(name) \
-static int test_## #name() __attribute__((section(".throwaway")))
+static int test_## #name(uint32_t artifact) __attribute__((section(".throwaway")))
 
 #endif
 
