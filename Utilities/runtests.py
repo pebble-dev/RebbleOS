4287ff84ecb5e31fb51bf77cf45843ee151aa112
Joshua Wise <joshua@joshuawise.com>
2020-01-26 22:41:16 -0800

    test runner: Construct a SPI image from a base filesystem image.

--- Utilities/runtests.py
+++ Utilities/runtests.py
@@ -9,17 +9,43 @@ import argparse
 import socket
 import sys
 import struct
+import tempfile
 from libpebble2.communication.transports.qemu.protocol import *
 from libpebble2.communication.transports.qemu import QemuTransport, MessageTargetQemu
 
 parser = argparse.ArgumentParser(description = "Test runner for RebbleOS.")
 parser.add_argument("--qemu", nargs = 1, required = True, help = "QEMU command, not including SPI flash image")
 parser.add_argument("--platform", nargs = 1, required = True, help = "platform name for SPI images")
+parser.add_argument("--fsofs", nargs = 1, required = True, help = "offset into SPI image of filesystem")
+parser.add_argument("--fssize", nargs = 1, required = True, help = "size of filesystem inside of SPI image")
+parser.add_argument("--flashsize", nargs = 1, required = True, help = "total SPI flash size")
+parser.add_argument("--resofs", nargs = 1, required = True, help = "offset into SPI image of resource pack")
+
 args = parser.parse_args()
 
 import shlex
 import subprocess
 import time
+import gzip
+
+def make_image(fs = None, keep = False):
+    newimg = tempfile.NamedTemporaryFile(delete = not keep)
+    newimg.write(b'\xff' * int(args.flashsize[0]))
+    
+    # Add in a resource pack.
+    with open(f'build/{args.platform[0]}_test/res/{args.platform[0]}_res.pbpack', 'rb') as resimg:
+      resdata = resimg.read()
+    newimg.seek(int(args.resofs[0]))
+    newimg.write(resdata)
+    
+    # Optionally, add a filesystem.
+    if fs is not None:
+        with gzip.open(fs, 'rb') as fsimg:
+            fsdata = fsimg.read()
+        newimg.seek(int(args.fsofs[0]))
+        newimg.write(fsdata)
+    
+    return newimg
 
 def launch_qemu(image):
     nargs = shlex.split(args.qemu[0])
@@ -38,23 +64,24 @@ def launch_qemu(image):
             time.sleep(0.1)
     if not launched:
         raise Exception("QEMU did not launch in five seconds")
-    
-    return qemu, skt
-  
-q, skt = launch_qemu(f'build/{args.platform[0]}_test/fw.qemu_spi.bin')
 
-t = QemuTransport(socket = skt)
+    print("Waiting for QEMU to wake up...")
+    t = QemuTransport(socket = skt)
+
+    target,data = t.read_packet()
+    assert(isinstance(data, QemuRebbleTest))
+    assert(isinstance(data.payload, QemuRebbleTestAlive))
+    
+    return qemu, t
+
+with make_image(f'tests/{args.platform[0]}/blank.gz') as f:
+    q, t = launch_qemu(f.name)
 
 def send_data(d):
     t.send_packet(QemuRebbleTest(payload = d), target = MessageTargetQemu(protocol = 100))
 
 def recv_data():
     return t.read_packet()
-
-print("Waiting for QEMU to wake up...")
-target,data = t.read_packet()
-assert(isinstance(data, QemuRebbleTest))
-assert(isinstance(data.payload, QemuRebbleTestAlive))
 
 print("Asking for test list...")
 send_data(QemuRebbleTestListRequest())
