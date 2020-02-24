a3d197fd7a38173d9d4b53a007729bc8e7c646d2
Joshua Wise <joshua@joshuawise.com>
2020-02-24 00:26:52 -0800

    Actually instantiate a subclass of Platform, so we cart around less stuff in the testplan.

--- tests/snowy/testplan.py
+++ tests/snowy/testplan.py
@@ -1,4 +1,17 @@
-from rebbletest import Test
+from rebbletest import Platform, Test
+
+class SnowyPlatform(Platform):
+    def __init__(self, qemu):
+        super().__init__(
+            name = 'snowy',
+            flashsize = 16777216,
+            resofs = 3670016,
+            fsofs = 4194304,
+            fssize = 12582912,
+            qemu = qemu
+        )
+
+ThisPlatform = SnowyPlatform
 
 testplan = [
     Test("Simple", testname = b'simple', golden = 42),
