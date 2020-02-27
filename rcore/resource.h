b2b03c6abace8da6a44a48b4d0eb34b1146ceb7d
Alice <AliceWeigt@gmail.com>
2020-02-27 12:25:28 -0800

    Improve Resource Sanity Checking (#96)
    Removed 200K hard limit for resources. Should be checking the size of available heap and comparing it to the resource size.

--- rcore/resource.h
+++ rcore/resource.h
@@ -18,6 +18,7 @@ size_t resource_size(ResHandle handle);
 size_t resource_load_byte_range(ResHandle res_handle, uint32_t start_offset, uint8_t *buffer, size_t num_bytes);
 void resource_load(ResHandle resource_handle, uint8_t *buffer, size_t max_length);
 void resource_load_file(ResHandleFileHeader resource_header_handle, uint8_t *buffer, size_t max_length, const struct file *file);
+bool _resource_is_sane(ResHandleFileHeader *res_handle);
 
 uint8_t *resource_fully_load_id_system(uint32_t resource_id);
 uint8_t *resource_fully_load_id_app(uint32_t resource_id);
