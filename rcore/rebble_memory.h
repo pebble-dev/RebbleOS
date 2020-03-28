43c31f25d5bc67137915191f62dcb6456183da4a
Joshua Wise <joshua@joshuawise.com>
2020-03-27 21:54:08 -0700

    memory: convert system_* memory allocators to magic allocator

--- rcore/rebble_memory.h
+++ rcore/rebble_memory.h
@@ -14,10 +14,7 @@
 #include <stdlib.h>
 #include "stdbool.h"
 
-void *system_calloc(size_t count, size_t size);
 void rblos_memory_init(void);
-void *system_malloc(size_t size);
-void system_free(void *mem);
 
 #define app_malloc malloc
 #define app_calloc calloc
@@ -55,3 +52,12 @@ void *mem_heap_realloc(struct mem_heap *heap, void *p, size_t newsz);
 void mem_heap_free(struct mem_heap *heap, void *p);
 void mem_thread_set_heap(struct mem_heap *heap);
 struct mem_heap *mem_thread_get_heap();
+
+/* Magic allocators. */
+void *malloc(size_t sz);
+void *calloc(size_t sz, size_t n);
+void *realloc(void *p, size_t new_size);
+void *remote_realloc(void *p, size_t new_size);
+void free(void *p);
+void remote_free(void *p);
+
