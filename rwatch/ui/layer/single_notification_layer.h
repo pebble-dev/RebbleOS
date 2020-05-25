6e364f7d7ece873e9decce0cbdc8484c6a47dfaf
Joshua Wise <joshua@joshuawise.com>
2020-05-24 22:12:22 -0700

    notifications: be able to change notifications in the single_layer

--- rwatch/ui/layer/single_notification_layer.h
+++ rwatch/ui/layer/single_notification_layer.h
@@ -17,7 +17,8 @@
     GBitmap *icon;
 } SingleNotificationLayer;
 
-extern void single_notification_layer_ctor(SingleNotificationLayer *l, rebble_notification *notif, GRect frame);
+extern void single_notification_layer_ctor(SingleNotificationLayer *l, GRect frame);
 extern void single_notification_layer_dtor(SingleNotificationLayer *l);
+extern void single_notification_layer_set_notification(SingleNotificationLayer *l, rebble_notification *notif);
 extern Layer *single_notification_layer_get_layer(SingleNotificationLayer *l);
 extern uint16_t single_notification_layer_height(SingleNotificationLayer *l);
