b77502c346b70fb5bae5b12a8dd115fa8a89b321
Joshua Wise <joshua@joshuawise.com>
2020-09-26 23:14:03 -0700

    tz: load time zones, too

--- Apps/System/settings_tz.c
+++ Apps/System/settings_tz.c
@@ -13,9 +13,95 @@
 
 static Window *_tzdir_window;
 static MenuLayer *_tzdir_menu;
-static StatusBarLayer *status_bar;
+static StatusBarLayer *_tzdir_status_bar;
+
+static Window *_tzsel_window;
+static MenuLayer *_tzsel_menu;
+static StatusBarLayer *_tzsel_status_bar;
+
 static struct fd tzfd;
-static int tzfdpos = 0xFFFF;
+static int tzfddirpos = 0xFFFF;
+static int tzfdselpos = 0xFFFF;
+
+static int tzdirsel = 0;
+
+/* Timezone (the timezone itself) selector logic. */
+
+static uint16_t _tzsel_menu_get_num_rows(MenuLayer *menu_layer, uint16_t section_index, void *context) {
+    int ntzs = 0;
+    int dirpos = 0;
+    char tzbuf[64];
+    union tzrec tzrec;
+    
+    tz_db_open(&tzfd);
+    for (dirpos = 0; dirpos <= tzdirsel; dirpos++)
+        (void) tz_db_nextdir(&tzfd, tzbuf, sizeof(tzbuf));
+    
+    tzfdselpos = 0;
+    while (tz_db_nexttz(&tzfd, tzbuf, sizeof(tzbuf), &tzrec) >= 0)
+        tzfdselpos++;
+    return tzfdselpos;
+}
+
+static void _tzsel_menu_draw_row(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *context) {
+    int tzn = cell_index->row;
+    char tzbuf[64];
+    union tzrec tzrec;
+    
+    if (tzn < tzfdselpos) {
+        int dirpos;
+        
+        tz_db_open(&tzfd);
+        for (int dirpos = 0; dirpos <= tzdirsel; dirpos++)
+            (void) tz_db_nextdir(&tzfd, tzbuf, sizeof(tzbuf));
+        tzfdselpos = 0;
+    }
+    
+    for (; tzfdselpos <= tzn; tzfdselpos++)
+        (void) tz_db_nexttz(&tzfd, tzbuf, sizeof(tzbuf), &tzrec);
+    
+    menu_cell_basic_draw(ctx, cell_layer, tzbuf, NULL, NULL);
+}
+
+static int16_t _tzsel_menu_get_cell_height(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context) {
+    return 44; /* hardcoded for basic_draw: can we get another one somewhere? */
+}
+
+static void _tzsel_menu_select_click(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context) {
+}
+
+static void _tzsel_window_load(Window *window)
+{
+    /* XXX: Some day this "menu plus a status bar" pattern should be
+     * factored out somewhere.  */
+    Layer *window_layer = window_get_root_layer(window);
+
+#ifdef PBL_RECT
+    _tzsel_menu = menu_layer_create(GRect(0, 16, DISPLAY_COLS, DISPLAY_ROWS - 16));
+#else
+#error round pebble not supported here
+#endif
+    menu_layer_set_callbacks(_tzsel_menu, NULL, (MenuLayerCallbacks) {
+        .get_num_rows = _tzsel_menu_get_num_rows,
+        .draw_row = _tzsel_menu_draw_row,
+        .get_cell_height = _tzsel_menu_get_cell_height,
+        .select_click = _tzsel_menu_select_click,
+    });
+    layer_add_child(window_layer, menu_layer_get_layer(_tzsel_menu));
+    menu_layer_set_click_config_onto_window(_tzsel_menu, window);
+
+    // Status Bar
+    _tzsel_status_bar = status_bar_layer_create();
+    layer_add_child(window_layer, status_bar_layer_get_layer(_tzsel_status_bar));
+}
+
+static void _tzsel_window_unload(Window *window)
+{
+    menu_layer_destroy(_tzsel_menu);
+    status_bar_layer_destroy(_tzsel_status_bar);
+}
+
+/* Timezone directory selector logic. */
 
 static uint16_t _tzdir_menu_get_num_rows(MenuLayer *menu_layer, uint16_t section_index, void *context) {
     int ntzs = 0;
@@ -24,7 +110,7 @@ static uint16_t _tzdir_menu_get_num_rows(MenuLayer *menu_layer, uint16_t section
     tz_db_open(&tzfd);
     while (tz_db_nextdir(&tzfd, tzbuf, sizeof(tzbuf)) >= 0)
         ntzs++;
-    tzfdpos = ntzs;
+    tzfddirpos = ntzs;
     
     return ntzs;
 }
@@ -33,16 +119,16 @@ static void _tzdir_menu_draw_row(GContext *ctx, const Layer *cell_layer, MenuInd
     int tzn = cell_index->row;
     char tzbuf[64];
     
-    if (tzn < tzfdpos) {
+    if (tzn < tzfddirpos) {
         tz_db_open(&tzfd);
-        tzfdpos = 0;
+        tzfddirpos = 0;
     }
     
-    for (; tzfdpos != tzn; tzfdpos++)
+    for (; tzfddirpos != tzn; tzfddirpos++)
         (void) tz_db_nextdir(&tzfd, tzbuf, sizeof(tzbuf));
     
     tz_db_nextdir(&tzfd, tzbuf, sizeof(tzbuf));
-    tzfdpos++;
+    tzfddirpos++;
     
     menu_cell_basic_draw(ctx, cell_layer, tzbuf, NULL, NULL);
 }
@@ -52,10 +138,16 @@ static int16_t _tzdir_menu_get_cell_height(struct MenuLayer *menu_layer, MenuInd
 }
 
 static void _tzdir_menu_select_click(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context) {
+    window_stack_push(_tzsel_window, false);
+    tzdirsel = cell_index->row;
+    menu_layer_reload_data(_tzsel_menu);
+    menu_layer_set_selected_index(_tzsel_menu, (MenuIndex) { .section = 0, .row = 0 }, MenuRowAlignTop, false);
 }
 
 static void _tzdir_window_load(Window *window)
 {
+    /* XXX: Some day this "menu plus a status bar" pattern should be
+     * factored out somewhere.  */
     Layer *window_layer = window_get_root_layer(window);
 
 #ifdef PBL_RECT
@@ -73,14 +165,14 @@ static void _tzdir_window_load(Window *window)
     menu_layer_set_click_config_onto_window(_tzdir_menu, window);
 
     // Status Bar
-    status_bar = status_bar_layer_create();
-    layer_add_child(window_layer, status_bar_layer_get_layer(status_bar));
+    _tzdir_status_bar = status_bar_layer_create();
+    layer_add_child(window_layer, status_bar_layer_get_layer(_tzdir_status_bar));
 }
 
 static void _tzdir_window_unload(Window *window)
 {
     menu_layer_destroy(_tzdir_menu);
-    status_bar_layer_destroy(status_bar);
+    status_bar_layer_destroy(_tzdir_status_bar);
 }
 
 void settings_tz_invoke() {
@@ -93,8 +185,16 @@ void settings_tz_init() {
         .load = _tzdir_window_load,
         .unload = _tzdir_window_unload,
     });
+
+    _tzsel_window = window_create();
+    window_set_window_handlers(_tzsel_window, (WindowHandlers) {
+        .load = _tzsel_window_load,
+        .unload = _tzsel_window_unload,
+    });
+
 }
 
 void settings_tz_deinit() {
     window_destroy(_tzdir_window);
+    window_destroy(_tzsel_window);
 }
