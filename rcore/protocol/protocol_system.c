a566d1a1dc1361e5e0e2f65a0c1ee8101ef524c8
Joshua Wise <joshua@joshuawise.com>
2020-10-04 17:30:12 -0700

    tz: running on hardware

--- rcore/protocol/protocol_system.c
+++ rcore/protocol/protocol_system.c
@@ -166,10 +166,11 @@ void protocol_time(const RebblePacket packet)
     }
     else if (data[0] == SetUTC)
     {
-        cmd_set_time_utc *utc = (cmd_set_time_utc *)&data[1];
+        cmd_set_time_utc *utc = (cmd_set_time_utc *)&data[0];
+        utc->utc_offset = ntohs(utc->utc_offset);
         SYS_LOG("FWPKT", APP_LOG_LEVEL_INFO, "Set UTC time for tz %s, offset %d", utc->tz_name, utc->utc_offset);
         rcore_set_time(ntohl(utc->unix_time));
-        rcore_set_utc_offset(utc->utc_offset);
+        rcore_set_utc_offset(((int)utc->utc_offset) * 60);
         rcore_set_tz_name(utc->tz_name, utc->pstr_len);
         rcore_tz_prefs_save();
     }
