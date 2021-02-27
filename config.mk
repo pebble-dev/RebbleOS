982d9277368b6ec82c84fd51bb6f47a5baa4e7b2
Alice Grey <alice@grey.systems>
2021-02-27 00:46:33 -0600

    Remove nrf52_ls013b7dh03 as it's not needed

--- config.mk
+++ config.mk
@@ -209,7 +209,6 @@ else
 include hw/chip/nrf52840/config.mk
 include hw/drivers/nrf52_buttons/config.mk
 include hw/drivers/nrf52_ls013b7dh05/config.mk
-include hw/drivers/nrf52_ls013b7dh03/config.mk
 include hw/drivers/nrf52_qspi_flash/config.mk
 include hw/drivers/nrf52_bluetooth/config.mk
 include hw/platform/asterix/config.mk
--- hw/drivers/nrf52_ls013b7dh03/config.mk
+++
@@ -1,3 +0,0 @@
-CFLAGS_driver_nrf52_ls013b7dh03 = -Ihw/drivers/nrf52_ls013b7dh03
-
-SRCS_driver_nrf52_ls013b7dh03 = hw/drivers/nrf52_ls013b7dh03/nrf52_ls013b7dh03.c
